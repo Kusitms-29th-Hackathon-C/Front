3c0769d22c77d23880060e7dc89f5d75ca10e930
hyunn522 <tjgus52213@gmail.com>
2024-04-06 21:51:51 +0900

    feat: react-calendar 기본 설정

---
+++ src/components/calendar/CustomCalendar.styles.tsx
@@ -0,0 +1,11 @@
+import { styled } from 'styled-components';
+import { Calendar } from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+
+export const calendarContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: min-content;
+  height: min-content;
+`;
