92004caf6f44582b6728a8e43547ef47ce592eb8
hyunn522 <tjgus52213@gmail.com>
2024-04-07 02:22:26 +0900

    feat: 내브바 클릭 시 이동

--- src/components/common/Navbar/Navbar.tsx
+++ src/components/common/Navbar/Navbar.tsx
@@ -2,16 +2,26 @@ import React from 'react';
 import styles from './Navbar.module.scss';
 
 import { Avatar, Logo, ProfileLine } from '../../../assets/images';
+import { useNavigate } from 'react-router-dom';
 
 const Navbar = () => {
+  const navigate = useNavigate();
+
   return (
     <div className={styles.container}>
       <div className={styles.rightWrapper}>
         <Logo />
         <div className={styles.menuContainer}>
-          <div className={styles.menuItems}>홈</div>
+          <div className={styles.menuItems} onClick={() => navigate('/')}>
+            홈
+          </div>
           <div className={styles.menuItems}>요약 훈련</div>
-          <div className={styles.menuItems}>상상 훈련</div>
+          <div
+            className={styles.menuItems}
+            onClick={() => navigate('/imagination')}
+          >
+            상상 훈련
+          </div>
         </div>
       </div>
       <div className={styles.profileWrapper}>
