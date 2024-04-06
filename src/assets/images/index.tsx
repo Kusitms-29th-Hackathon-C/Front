fc83199132fd20f55948a952380f786b9eff898d
hyunn522 <tjgus52213@gmail.com>
2024-04-07 02:49:40 +0900

    feat: 헤더 완성

--- src/assets/images/index.tsx
+++ src/assets/images/index.tsx
@@ -2,3 +2,4 @@ export { ReactComponent as Logo } from './Logo.svg';
 export { ReactComponent as Avatar } from './Avatar.svg';
 export { ReactComponent as ProfileLine } from './ProfileLine.svg';
 export { ReactComponent as PointLine } from './PointLine.svg';
+export { ReactComponent as Header } from './Group-45.svg';
