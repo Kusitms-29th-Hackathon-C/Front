d4d30b3be7ec07559f20e28418c6c2c659545e22
hyunn522 <tjgus52213@gmail.com>
2024-04-06 23:27:42 +0900

    feat: footer 레이아웃 구현

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import ScrollToTop from './components/common/ScrollToTop';
 import Home from './pages/Home/Home';
 import Navbar from './components/common/Navbar/Navbar';
+import Footer from './components/footer/Footer';
 
 const Router = () => {
   return (
@@ -11,6 +12,7 @@ const Router = () => {
         <Routes>
           <Route path='/' element={<Home />} />
         </Routes>
+        <Footer />
       </ScrollToTop>
     </BrowserRouter>
   );
