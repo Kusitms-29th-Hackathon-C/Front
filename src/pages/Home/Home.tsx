7d4bad1aa9aeb3dc094e063293303832c47d0771
이효원 <ymj07168@gmail.com>
2024-04-06 22:56:28 +0900

    feat: 섹션 레이아웃 컴포넌트 구현

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import styles from './Home.module.scss';
+import Section from '../../components/common/Section/Section';
 
 const Home = () => {
-  return <div className={styles.container}>home</div>;
+  return (
+    <>
+      <Section title={'작성중인 글'}>
+        <div>dddd</div>
+      </Section>
+    </>
+  );
 };
 
 export default Home;
