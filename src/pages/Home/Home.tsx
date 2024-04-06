bdd8cc867e79d30818c200699d6edc7780c2a481
hyunn522 <tjgus52213@gmail.com>
2024-04-07 01:57:49 +0900

    feat: 상상력 top5 구현

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,17 +1,27 @@
 import React from 'react';
 import styles from './Home.module.scss';
+import CustomCalendar from '../../components/calendar/CustomCalendar';
+import Label from '../../components/common/label/Label';
+import Section from '../../components/common/Section/Section';
+import PostListItem from '../../components/PostListItem/PostListItem';
 import PostSection from '../../components/PostSection/PostSection';
 import HomeSummary from '../../components/homeSummary/HomeSummary';
 import Footer from '../../components/common/footer/Footer';
+import HomeTop from '../../components/HomeTop/HomeTop';
 import Attendance from '../../components/attendance/Attendance';
 
 const Home = () => {
   return (
     <>
       <div className={styles.container}>
-        <Attendance week={3} day={3} />
-        <PostSection />
-        <HomeSummary />
+        <div className={styles.subContainer}>
+          <PostSection />
+          <HomeSummary />
+        </div>
+        <div className={styles.subContainer}>
+          <HomeTop />
+          <Attendance week={3} day={3} />
+        </div>
       </div>
       <Footer />
     </>
