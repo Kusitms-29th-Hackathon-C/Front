d7d95a9984ec306a542c0603217a012dd9d1a0fd
이효원 <ymj07168@gmail.com>
2024-04-07 03:48:15 +0900

    feat: 상상력 글쓰기 결과 구현

--- src/assets/images/index.tsx
+++ src/assets/images/index.tsx
@@ -5,3 +5,5 @@ export { ReactComponent as PointLine } from './PointLine.svg';
 export { ReactComponent as Header } from './Group-45.svg';
 export { ReactComponent as SummaryOthers } from './summary_others.svg';
 export { ReactComponent as WriteCharacter } from './write-imagination.svg';
+export { ReactComponent as Like } from './Like.svg';
+export { ReactComponent as Stroke } from './Stroke.svg';
