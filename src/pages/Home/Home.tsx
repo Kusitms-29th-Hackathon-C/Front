43154d8aba63b427ab6b152a25b192d46e211b7e
이효원 <ymj07168@gmail.com>
2024-04-06 23:41:08 +0900

    feat: 리스트 아이템 컴포넌트 구현

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,13 +3,19 @@ import styles from './Home.module.scss';
 import CustomCalendar from '../../components/calendar/CustomCalendar';
 import Label from '../../components/common/label/Label';
 import Section from '../../components/common/Section/Section';
+import PostListItem from '../../components/PostListItem/PostListItem';
 
 const Home = () => {
   return (
     <>
       <div className={styles.container}>
         <Section title={'작성중인 글'}>
-          <div>dddd</div>
+          <PostListItem
+            type={'summary'}
+            count={1000}
+            level={2}
+            content={'글 요약을하고 있어요 글 요약이에요 글 요약 ...'}
+          />
         </Section>
       </div>
     </>
