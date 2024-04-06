31afa93f52aa2ef4e11858c3ef992b083a945673
hyunn522 <tjgus52213@gmail.com>
2024-04-06 22:27:30 +0900

    feat: LargeButton 색에 따라 컴포넌트 구현

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import styles from './Home.module.scss';
 import CustomCalendar from '../../components/calendar/CustomCalendar';
+import LargeButton from '../../components/common/button/LargeButton';
 
 const Home = () => {
   return (
     <>
       <div className={styles.container}>home</div>
       <CustomCalendar />
+      <LargeButton type='0' />
     </>
   );
 };
